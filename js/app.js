c0d2f182da9a69026424285e8d6ffe807dbf9798
Liam North <liam_north@outlook.com>
2017-09-18 16:23:31 +0100

    Prints red when the selected square corresponds to a boat class square on the other player's board

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(function() {
 	var player1;
 	var player2;
 
-	generatePlayerDisplay($("#player11"), $("#player12"));
-	generatePlayerDisplay($("#player21"), $("#player22"));
+	generatePlayerDisplay($("#player11"), $("#player12"), $("#player22"));
+	generatePlayerDisplay($("#player21"), $("#player22"), $("#player12"));
 
 
 	$("#show1").click(function() {
@@ -18,13 +18,13 @@ $(function() {
 		$("#player2").show();
 	});
 
-	function generatePlayerDisplay(display1, display2) {
-		generateBoard(display1);
+	function generatePlayerDisplay(display1, display2, display3) {
+		generateBoard(display1, display3);
 		generateBoard(display2);
 		addBoats(display2);
 	}
 
-	function generateBoard(display) {
+	function generateBoard(display, display2) {
 		for(var i = 0; i < 121; i++) {
 			$(display).append($("<li></li>"));
 		}
@@ -37,24 +37,27 @@ $(function() {
 			} else if(index%11 === 0) {
 				$(li).html(letters[index/11-1]);
 			} else {
-				$(li).addClass("active").one("click", function() {
-					switch(which){
-						case true:
-							red(li);
-							which = false;
-							break;
-						case false:
-							white(li);
-							which = true;
-							break;
+				$(li).one("click", function() {
+					if(hitCheck(index, display2) === true) {
+						red($(li));
+					} else {
+						white($(li));
 					}
+					
 				});
 			}
 		});
 
 	}
 
-	
+	function hitCheck(num, display) {
+		var lis = $(display).children()
+		if($(lis[num]).hasClass("boat") === true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 
 	function addBoats(display) {
