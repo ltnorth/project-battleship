e448b8231f81523f5b88c7538bc6fef569ce12db
Liam North <liam_north@outlook.com>
2017-09-18 15:25:22 +0100

    Added another player screen

--- js/app.js
+++ js/app.js
@@ -1,48 +1,64 @@
 $(function() {
+	$(".display").hide();
 	var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-	var which = true;
-	var squareIndex = [];
+	var player1;
+	var player2;
 
-	for(var i = 0; i < 121; i++) {
-		$("ul").append($("<li></li>"));
-		squareIndex.push(i);
-	}
+	generateBoard($("#player1"));
+	generateBoard($("#player2"));
 
-	$("li").each(function (index, li) {
-		if(index === 0){
-			$(li).html("x");
-		} else if(index < 11) {
-			$(li).html(index);
-		} else if(index%11 === 0) {
-			$(li).html(letters[index/11-1]);
-		} else {
-			$(li).addClass("active").one("click", function() {
-				switch(which){
-					case true:
-						red($(li));
-						which = false;
-						break;
-					case false:
-						white($(li));
-						which = true;
-						break;
-				}
-			});
-		}
+
+	$("#show1").click(function() {
+		$("#player2").hide();
+		$("#player1").show();
+	});
+	$("#show2").click(function() {
+		$("#player1").hide();
+		$("#player2").show();
 	});
 
-	addBoats();
+
+	function generateBoard(display) {
+		for(var i = 0; i < 121; i++) {
+			$(display).append($("<li></li>"));
+		}
+	
+		$(display).children().each(function (index, li) {
+			if(index === 0){
+				$(li).html("x");
+			} else if(index < 11) {
+				$(li).html(index);
+			} else if(index%11 === 0) {
+				$(li).html(letters[index/11-1]);
+			} else {
+				$(li).addClass("active").one("click", function() {
+					switch(which){
+						case true:
+							red(li);
+							which = false;
+							break;
+						case false:
+							white(li);
+							which = true;
+							break;
+					}
+				});
+			}
+		});
+
+		addBoats(display);
+	}
 
 
-	function addBoats() {
-		var lis = $("li");
+	function addBoats(display) {
+		var lis = $(display).children();
 		var boats = [2, 3, 3, 4, 5];
 		var check = false;
 		while(boats.length !== 0) {
 			var boat = selectNumber(boats);
 			while(check === false){
 				var position = randomGridPos(boat);
-				if(verify(boat, position) === true){
+				if(verify(boat, position, display) === true){
 					for(var i = 0; i < boat; i++) {
 						$(lis[position + i]).addClass("boat");
 					}
@@ -53,8 +69,8 @@ $(function() {
 		}
 	}
 
-	function verify(boat, position) {
-		var lis = $("li");
+	function verify(boat, position, display) {
+		var lis = $(display).children();
 		var count = 0;
 		for(var i = 0; i < boat; i++) {
 			if($(lis[position + i]).hasClass("boat") === true) {
